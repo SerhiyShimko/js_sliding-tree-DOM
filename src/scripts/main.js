d1e32d66371110ec9e60ca38db4d792e5d05a69e
Serhii Shimko <shimkosergij2010@gmail.com>
2026-02-25 17:31:33 +0200

    completed the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write code here
+const li = Array.from(document.querySelectorAll('li'));
+
+for (const list of li) {
+  const span = document.createElement('span');
+
+  span.textContent = list.firstChild.textContent.trim();
+  list.removeChild(list.firstChild);
+
+  list.prepend(span);
+
+  span.addEventListener('click', (eve) => {
+    const spanTarget = eve.target;
+
+    if (spanTarget instanceof HTMLElement) {
+      const next = list.querySelector('ul');
+
+      if (next instanceof HTMLElement) {
+        if (next.hasAttribute('hidden')) {
+          next.removeAttribute('hidden');
+
+          return;
+        }
+        next.setAttribute('hidden', '');
+      }
+    }
+  });
+}
